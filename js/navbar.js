2f788212bf11da7467bf04c76e3db90ac0ab58a9
Andrew Kononenko <wastardy.k@gmail.com>
2025-02-28 14:36:19 +0200

    add logic for small and mid displays while scrolling

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,27 @@
 //#region Functions
 const userScroll = () => {
     const navbar = document.querySelector('.navbar');
-    if (!navbar) return;
+    
+    if (!navbar) {
+        console.error("Navbar not found!");
+        return;
+    }
+
+    if (window.innerWidth >= 320 && window.innerWidth <= 992) {
+        navbar.classList.add('navbar-scroll-background-sm');
+    }
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 55) {
-            navbar.classList.remove('navbar-scroll-background');
-            navbar.classList.add('navbar-background');
-        } else {
+            // navbar.classList.remove('navbar-scroll-background');
             navbar.classList.add('navbar-scroll-background');
-            navbar.classList.remove('navbar-background');
+        } else {
+            // navbar.classList.add('navbar-scroll-background');
+            navbar.classList.remove('navbar-scroll-background');
+            
+            if (window.innerWidth > 992) {
+                navbar.classList.remove('navbar-scroll-background-sm');
+            }
         }
     });
 }
@@ -18,7 +30,7 @@ const observer = new MutationObserver((mutations, obs) => {
     const navbar = document.querySelector('.navbar');
     if (navbar) {
         userScroll();
-        obs.disconnect();
+        obs.disconnect();  // Зупинити спостереження після того, як navbar знайдено
     }
 });
 
@@ -39,6 +51,6 @@ function clickOutsideNavbar(event) {
 //#endregion
 
 //#region Event Listeners
-// document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', userScroll);
 document.addEventListener('click', clickOutsideNavbar);
 //#endregion
